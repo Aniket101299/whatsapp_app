183cddf16cd665e72e87750e0028e4ac5ab04203
Aniket101299 <91543059+Aniket101299@users.noreply.github.com>
2022-07-27 16:55:21 +0530

    create basic skeleton for application sidebar and chat components

---
+++ src/Sidebar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import "./Sidebar.css";
+
+function Sidebar() {
+  return (
+    <div className="sidebar">
+      <h1>I am a sidebar</h1>
+      {/* <div className='sidebar_header'></div> 
+        <div className='sidebar_headerRight'></div> */}
+    </div>
+  )
+}
+
+export default Sidebar;
