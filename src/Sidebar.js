fa19c2e29ff278e1320c1053a67f9c7e5d0b7c87
Aniket101299 <91543059+Aniket101299@users.noreply.github.com>
2022-07-27 18:59:58 +0530

    search box has implemented successfully

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -4,6 +4,7 @@ import  DonutLargeIcon  from '@material-ui/icons/DonutLarge';
 import { Avatar, IconButton } from '@material-ui/core';
 import ChatIcon from "@material-ui/icons/Chat";
 import MoreVertIcon from "@material-ui/icons/MoreVert";
+import { SearchOutlined } from "@material-ui/icons";
 
 function Sidebar() {
   return (
@@ -21,7 +22,14 @@ function Sidebar() {
         <MoreVertIcon />
         </IconButton>
       </div>
-     </div>    
+     </div>  
+
+      <div className="sidebar_search">
+        <div className="sidebar_searchContainer">
+            <SearchOutlined />
+            <input placeholder='Search or start new chat' type="text"/>
+        </div>
+      </div>  
     </div>
   )
 }
