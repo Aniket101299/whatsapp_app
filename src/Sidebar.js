8e94c28b8b75deedcdb99f4bb5a016f225c06158
Aniket101299 <91543059+Aniket101299@users.noreply.github.com>
2022-07-27 18:33:24 +0530

    using material ui icons sidebar header part is done

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,12 +1,27 @@
 import React from 'react';
 import "./Sidebar.css";
+import  DonutLargeIcon  from '@material-ui/icons/DonutLarge';
+import { Avatar, IconButton } from '@material-ui/core';
+import ChatIcon from "@material-ui/icons/Chat";
+import MoreVertIcon from "@material-ui/icons/MoreVert";
 
 function Sidebar() {
   return (
     <div className="sidebar">
-      <h1>I am a sidebar</h1>
-      {/* <div className='sidebar_header'></div> 
-        <div className='sidebar_headerRight'></div> */}
+      <div className='sidebar_header'>
+        <Avatar src="https://tse3.mm.bing.net/th?id=OIP.OJMH1q9cS-AFG308T5O_PQHaLH&pid=Api&P=0"/>
+      <div className='sidebar_headerRight'>
+        <IconButton>
+        <DonutLargeIcon />
+        </IconButton>
+        <IconButton>
+        <ChatIcon />
+        </IconButton>
+        <IconButton>
+        <MoreVertIcon />
+        </IconButton>
+      </div>
+     </div>    
     </div>
   )
 }
