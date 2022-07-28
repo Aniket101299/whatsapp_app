9c9c8f1dcf51c94aed0bc2b3817fe62d6a6fa780
Aniket101299 <91543059+Aniket101299@users.noreply.github.com>
2022-07-28 08:02:26 +0530

    frontend part is done successfully by completing right side individual chat section with sender & receiver chats, material ui icons

--- src/Chat.js
+++ src/Chat.js
@@ -2,6 +2,8 @@ import { Avatar, IconButton } from '@material-ui/core';
 import React from 'react';
 import "./Chat.css";
 import { AttachFile, MoreVert, SearchOutlined } from "@material-ui/icons";
+import InsertEmoticonIcon from "@material-ui/icons/InsertEmoticon";
+import MicIcon from "@material-ui/icons/Mic";
 
 function Chat() {
   return (
@@ -25,8 +27,37 @@ function Chat() {
         <MoreVert />
         </IconButton>
         </div>
-
       </div>
+
+    <div className="chat_body">
+        <p className='chat_message'>
+            <span className="chat_name">sonny</span>
+            This is a message
+            <span className="chat_timestamp">{new Date().toUTCString()}</span>
+        </p>
+
+        <p className='chat_message chat_receiver'>
+            <span className="chat_name">sonny</span>
+            This is a message
+            <span className="chat_timestamp">{new Date().toUTCString()}</span>
+        </p>
+
+        <p className='chat_message'>
+            <span className="chat_name">sonny</span>
+            This is a message
+            <span className="chat_timestamp">{new Date().toUTCString()}</span>
+        </p>
+      </div>
+
+        <div className='chat_footer'>
+            <InsertEmoticonIcon />
+            <form>
+                <input placeholder='Type a message' type="text"/>
+                <button type='submit'>Send a message</button>
+            </form>
+            <MicIcon />
+        </div>
+        
     </div>
   );
 }
